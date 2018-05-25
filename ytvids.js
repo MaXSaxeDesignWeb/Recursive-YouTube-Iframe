fd2bdb47e9e22ec0998a2c74292a04d3e9e7ee39
MaX Falstein <max@saxe-falstein.co.uk>
2018-05-25 19:53:36 +0100

    Add/update origins

--- ytvids.js
+++ ytvids.js
@@ -1,25 +1,32 @@
 var uri = ["FPI9Rt1PIkY", "W282G4FiS9I", "0FLHal964uM", "Dx8VTOGhIXo", "J_EuWgOK88I", "ST6ojbdhv7Q", "0BWji9hBlO0", "qfmTR_CLYEo", "T1KbB0BheZs", "Zjn2F1uKP1U", "10wqmdu5S7Y", "liAzX0cO5Tg"];
-var embed = [true, false, false, false, false, false, false, false, false, false, false, false];
-var origin = "http://yt.maxfalstein.co.uk";
+var autoplay = [true, false, false, false, false, false, false, false, false, false, false, false];
+// var autoplay = [true, false, true, false, true, false, true, false, true, false, true, false];
+
+// var origin = "origin=http://yt.maxfalstein.co.uk/";
+var href = window.location.href;
+var origin = "origin=".concat(href);
+var autoplayText = "?autoplay=1&";
+var questionMark = "?autoplay=0&";
+var ytEmbed = "https://www.youtube.com/embed/";
 iframe();
 function iframe() {
-	var i;
-	var curi, furi = "";
-	for (i = 0; i < uri.length; i++) {
-	  furi = "https://www.youtube.com/embed/" + uri[i];
-    if (embed[i] == true) {
-      curi = furi + "?autoplay=1&" + origin;
-    } else {
-      curi = furi + "?" + origin;
+  var i;
+  var curi, furi = "";
+  for (i = 0; i < uri.length; i++) {
+    furi = ytEmbed.concat(uri[i]);
+    if (autoplay[i] == true) {
+      curi = furi.concat(autoplayText).concat(origin);
+	} else {
+      curi = furi.concat(questionMark).concat(origin);
     }
-	  var iframe = document.createElement('iframe');
-	  iframe.src = encodeURI(curi);
-	  iframe.style.width = "560px";
-	  iframe.style.height = "315px";
+    var iframe = document.createElement('iframe');
+    iframe.src = encodeURI(curi);
+    iframe.style.width = "560px";
+    iframe.style.height = "315px";
     iframe.frameborder = "0";
     iframe.allow ="autoplay; encrypted-media; allowfullscreen";
-		iframe.setAttribute('allowFullScreen', '')
-	  document.getElementById("YTVids").appendChild(iframe);
-	  console.log('iframe.contentWindow =', iframe.contentWindow);
-	}
+    iframe.setAttribute('allowFullScreen', '')
+    document.getElementById("YTVids").appendChild(iframe);
+    console.log('iframe.contentWindow =', iframe.contentWindow);
+  }
 }
