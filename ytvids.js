b49711ff04750df113afc514ecc789576c4dfc88
MaX Falstein <MaXFalstein@users.noreply.github.com>
2018-07-20 14:15:53 +0100

    Remove old test code

--- ytvids.js
+++ ytvids.js
@@ -1,15 +1,14 @@
 var uri = ["FPI9Rt1PIkY", "W282G4FiS9I", "0FLHal964uM", "Dx8VTOGhIXo", "J_EuWgOK88I", "ST6ojbdhv7Q", "0BWji9hBlO0", "qfmTR_CLYEo", "T1KbB0BheZs", "Zjn2F1uKP1U", "10wqmdu5S7Y", "liAzX0cO5Tg"];
 var autoplay = [true, false, false, false, false, false, false, false, false, false, false, false];
-// var autoplay = [true, false, true, false, true, false, true, false, true, false, true, false];
-
-// var origin = "origin=http://yt.maxfalstein.co.uk/";
+var delay = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000]
 var href = window.location.href;
 var origin = "origin=".concat(href);
 var autoplayText = "?autoplay=1&";
 var questionMark = "?autoplay=0&";
 var ytEmbed = "https://www.youtube.com/embed/";
+
 iframe();
-var chunky = iframe.curi();
+
 function iframe() {
   var i;
   var curi, furi = "";
@@ -17,7 +16,7 @@ function iframe() {
     furi = ytEmbed.concat(uri[i]);
     if (autoplay[i] == true) {
       curi = furi.concat(autoplayText).concat(origin);
-	} else {
+		} else {
       curi = furi.concat(questionMark).concat(origin);
     }
     var iframe = document.createElement('iframe');
@@ -29,8 +28,9 @@ function iframe() {
     iframe.setAttribute('allowFullScreen', '')
     document.getElementById("YTVids").appendChild(iframe);
     console.log('iframe.contentWindow =', iframe.contentWindow);
+		setTimeout(check, delay[i]);
   }
-  function curi() {
-	return curi;
+	function printCuri() {
+	  console.log(curi);
   }
 }
